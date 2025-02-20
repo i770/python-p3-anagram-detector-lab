020128ff5482f0e329d36c0cc4edccea7366404e
i770 <diegomudaida@gmail.com>
2025-02-20 03:22:17 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+        
+        for word in word_list:
+            if sorted(word.lower()) == sorted_word and word.lower() != self.word:
+                matches.append(word)
+                
+        return matches
